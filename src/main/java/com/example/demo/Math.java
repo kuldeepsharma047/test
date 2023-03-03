41f4f8e4db0c6ee97a81eb5dd7410463f08caf98
kuldeepsharma047 <kuldeep.sharma@impressico.com>
2023-03-03 18:18:16 +0530

    Math modified

--- src/main/java/com/example/demo/Math.java
+++ src/main/java/com/example/demo/Math.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
 public class Math {
-
-    public int add(int a, int b) {
+    public static int add(int a, int b) {
         return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
     }
 }
